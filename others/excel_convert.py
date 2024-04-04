a24a23d3f0d05f0f3ed5d34993d5660cd1ea82f1
Arjun A I <arjunai5768@gmail.com>
2024-04-03 17:00:32 -0700

    update

--- others/excel_convert.py
+++ others/excel_convert.py
@@ -4,7 +4,7 @@ import tabula
   
 # Read PDF File 
 # this contain a list 
-df = tabula.read_pdf("PDF File Path", pages = 1)[0] 
+df = tabula.read_pdf(r"C:\Users\arjun\Code\miniproj\Miniproject\ktu_calender.pdf", pages = 1)[0] 
   
 # Convert into Excel File 
-df.to_excel('Excel File Path') 
+df.to_excel('./ktu_calender.xlsx', index = False) 
